a2b292084ad570827bd2384ca0b6fb694056fb48
Thomas Bryce Kelly <tbrycekelly@gmail.com>
2024-11-12 12:29:56 -0900

    Add readme.md

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
 # SimpleBathy
- An add on package for SimpleMapper to provide bathymetric data and capabilities.
+
+An add on package for [SimpleMapper](https://github.com/tbrycekelly/SimpleMapper) to provide bathymetric data and capabilities.
+
+You may also be interested in a color palette package available based on the excellent _batlow_ set of palettes for scientific data presentation: [batlow package](https://github.com/tbrycekelly/batlow).
+
+## Getting Started
+
+The easiest way to install the package (until I get around to adding this to CRAN) is via *devtools*.
+
+If you don't have *devtools*, then install that first via CRAN:
+
+``` r
+install.pacakges('devtools')
+```
+
+Now you can use *devtools* to install R packages from Github, such as *SimpleMapper*:
+
+``` r
+devtools::install_github('tbrycekelly/SimpleMapper') # If you don't have it already.
+devtools::install_github('tbrycekelly/SimpleBathy')
+```
+
+All done! Now *SimpleBathy* can be loaded just like any other package: `library(SimpleBathy)`.
+
+------------------------------------------------------------------------
+
+#### Examples
+
+<img src="figures/Map1.png"/>
+
+```r
+map = plotBasemap(coastline4, scale = 500, lat = 60, lon = -147)
+map = addBathy(map, zlim = c(-3e3, 0), ztrim = c(-3e3, NA), pal = pals::cubicl(128))
+map = addRelief(map)
+map = addCoastline(map)
+map = addLatitude(map)
+map = addLongitude(map)
+```
+
+------------------------------------------------------------------------
+
+<img src="figures/Map2.png"/>
+
+```r
+map = plotBasemap(coastline4, scale = 1000, lat = 60, lon = -147)
+map = addBathy(map, zlim = c(-3e3, 0), ztrim = c(-3e3, NA), pal = rev(pals::brewer.blues(32)))
+map = addCoastline(map)
+```
+
+------------------------------------------------------------------------
+
+<img src="figures/Map3.png"/>
+
+```r
+map = plotBasemap(coastline3, scale = 2000, lat = 60, lon = -147)
+map = addBathy(map, zlim = c(-3e3, 0), ztrim = c(-3e3, NA), pal = greyscale(8))
+map = addRelief(map)
+map = addCoastline(map)
+```
+
+------------------------------------------------------------------------
+
+<img src="figures/Map4.png"/>
+
+```r
+map = plotBasemap(coastline3, scale = 8000, lat = 90, lon = 0)
+map = addBathy(map, zlim = c(-3e3, 0), ztrim = c(-3e3, NA), pal = pals::cubicl(8))
+map = addRelief(map, zlim = c(100, 1000))
+map = addCoastline(map)
+```
+
+------------------------------------------------------------------------
+
+<img src="figures/Map5.png"/>
+
+```r
+map = plotBasemap(coastline4, scale = 500, lat = 59, lon = -148)
+map = addBathy(map, zlim = c(-5e3, 0), ztrim = c(-5e3, NA), pal = pals::ocean.deep(16), trim = F)
+map = addRelief(map, zlim = c(100, 1000))
+map = addCoastline(map)
+```
+
+------------------------------------------------------------------------
+
+## About
--- create_data/tests.R
+++ create_data/tests.R
@@ -1,48 +1,44 @@
 library(SimpleMapper)
 library(SimpleBathy)
 
+png('figures/Map1.png', width = 800, height = 600, type = 'cairo')
 map = plotBasemap(coastline4, scale = 500, lat = 60, lon = -147)
-map = addBathy(map, zlim = c(-3e3, 0), ztrim = c(-3e3, NA), pal = pals::cubicl(8))
+map = addBathy(map, zlim = c(-3e3, 0), ztrim = c(-3e3, NA), pal = pals::cubicl(128))
 map = addRelief(map)
 map = addCoastline(map)
+map = addLatitude(map)
+map = addLongitude(map)
+dev.off()
 
+
+png('figures/Map2.png', width = 800, height = 600, type = 'cairo')
 map = plotBasemap(coastline4, scale = 1000, lat = 60, lon = -147)
-map = addBathy(map, zlim = c(-3e3, 0), ztrim = c(-3e3, NA), pal = pals::cubicl(8))
-map = addRelief(map)
+map = addBathy(map, zlim = c(-3e3, 0), ztrim = c(-3e3, NA), pal = rev(pals::brewer.blues(32)))
 map = addCoastline(map)
+dev.off()
 
+
+png('figures/Map3.png', width = 800, height = 600, type = 'cairo')
 map = plotBasemap(coastline3, scale = 2000, lat = 60, lon = -147)
-map = addBathy(map, zlim = c(-3e3, 0), ztrim = c(-3e3, NA), pal = pals::cubicl(8))
+map = addBathy(map, zlim = c(-3e3, 0), ztrim = c(-3e3, NA), pal = greyscale(8))
 map = addRelief(map)
 map = addCoastline(map)
-
-map = plotBasemap(coastline4, scale = 3000, lat = 60, lon = -147)
-map = addBathy(map, zlim = c(-3e3, 0), ztrim = c(-3e3, NA), pal = pals::cubicl(8))
-map = addRelief(map)
-map = addCoastline(map)
-
-map = plotBasemap(coastline2, scale = 8000, lat = 60, lon = -147)
-map = addBathy(map, zlim = c(-3e3, 0), ztrim = c(-3e3, NA), pal = pals::cubicl(8))
-map = addRelief(map)
-map = addCoastline(map)
+dev.off()
 
 
-
+png('figures/Map4.png', width = 800, height = 600, type = 'cairo')
 map = plotBasemap(coastline3, scale = 2000, lat = 90, lon = 0)
 map = addBathy(map, zlim = c(-3e3, 0), ztrim = c(-3e3, NA), pal = pals::cubicl(8))
-map = addRelief(map, zlim = c(100, 1000))
 map = addCoastline(map)
-
-map = plotBasemap(coastline3, scale = 4000, lat = 90, lon = 0)
-map = addBathy(map, zlim = c(-3e3, 0), ztrim = c(-3e3, NA), pal = pals::cubicl(8))
-map = addRelief(map, zlim = c(100, 1000))
-map = addCoastline(map)
+dev.off()
 
 
+png('figures/Map4.png', width = 800, height = 600, type = 'cairo')
 map = plotBasemap(coastline3, scale = 8000, lat = 90, lon = 0)
 map = addBathy(map, zlim = c(-3e3, 0), ztrim = c(-3e3, NA), pal = pals::cubicl(8))
 map = addRelief(map, zlim = c(100, 1000))
 map = addCoastline(map)
+dev.off()
 
 
 map = plotBasemap(coastline3, scale = 8000, lat = 90, lon = 180.1234325)
@@ -80,8 +76,9 @@ map = addRelief(map, zlim = c(100, 1000))
 map = addCoastline(map)
 
 
-
+png('figures/Map5.png', width = 800, height = 600, type = 'cairo')
 map = plotBasemap(coastline4, scale = 500, lat = 59, lon = -148)
 map = addBathy(map, zlim = c(-5e3, 0), ztrim = c(-5e3, NA), pal = pals::ocean.deep(16), trim = F)
 map = addRelief(map, zlim = c(100, 1000))
 map = addCoastline(map)
+dev.off()
